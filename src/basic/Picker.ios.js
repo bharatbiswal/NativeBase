a7e6bbd10799b4e8d486c6452dd5432db8b9617c
Bharat Biswal <bharat@mobiona.com>
2018-11-30 08:12:08 +0530

    Added placeholder which will not show up in picker

--- src/basic/Picker.ios.js
+++ src/basic/Picker.ios.js
@@ -201,8 +201,10 @@ class PickerNB extends Component {
             <FlatList
               data={this.state.dataSource}
               keyExtractor={(item, index) => String(index)}
-              renderItem={({ item }) => (
-                <ListItem
+              renderItem={({ item }) => {
+                if ((item.props.placeholder == undefined) || (item.props.placeholder == false)) {
+                  return (
+                    <ListItem
                   selected={item.props.value === this.props.selectedValue}
                   button
                   style={((item.props.disabled != undefined) && (item.props.disabled == true))? this.props.itemDisabledStyle : this.props.itemStyle}
@@ -230,7 +232,11 @@ class PickerNB extends Component {
                       )}
                   </Right>
                 </ListItem>
-              )}
+                  );
+                } else {
+                  return (<View></View>);
+                }
+            }}
             />
           </Container>
         </Modal>
